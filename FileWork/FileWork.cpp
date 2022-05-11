458f2b3a301d1e62e742160b0dc9ed0d97191df4
Exolan <97833724+Exolan@users.noreply.github.com>
2022-05-11 11:29:51 +0300

    Add files via upload

---
+++ FileWork/FileWork.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    ifstream file("FileWork.txt");
+
+    char fopen[255];
+    int n = 0;
+    struct item
+    {
+        char name[30];
+        int count[4];
+        int fabric[4];
+    };
+
+    item items[8];
+
+    if (file.is_open()) {
+        
+        while (!file.eof()) {
+            file.getline(fopen, 255, '\n');
+            cout << fopen << endl;
+            items[n].name = fopen;
+            n++;
+        }
+        file.close();
+        cout << fopen << endl;
+    }
+
+}
+
